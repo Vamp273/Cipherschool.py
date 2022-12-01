5cc265aea3a99a6d8def08f19d5e92d890547702
VAISHNAVI\91630 <vaishnavi918gupta@gmail.com>
2022-12-01 22:28:11 +0530

    conditional statement

---
+++ class5_cipherschool.py
@@ -0,0 +1,47 @@
+a = 65
+print(isinstance(a,object))
+
+b=43
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+
+x= "VaishnaviGupta"
+y = x*10
+print(y)
+
+a>b
+a<b
+
+# 0 if false and all positive integers is true.
+
+g = bool(0)
+print(g)
+
+h = bool(1)
+print(h)
+
+"""
+SHORT CURCUTING
+
+a = "jatin"
+b = 6
+if b is True:
+    b = a or b
+    print(b)
+elif b is False:
+    a = a or b
+    print(a)
+elif b is True:
+    a = a and b
+    print(a)
+elif b is False:
+    b = a and b
+    print(b)
+
+true or b = true
+false or b = b
+false and b = false
+"""
