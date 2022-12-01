1a0c523fc525c617c2b2b0f354581b6dedd3c05f
VAISHNAVI\91630 <vaishnavi918gupta@gmail.com>
2022-12-01 22:30:27 +0530

    for loop questions

---
+++ class8_cipherschool.py
@@ -0,0 +1,70 @@
+n = 5 
+for i in range(n):
+    for j in range(n):
+        print(i, end=" ")
+    print()
+
+z = 5 
+for i in range(z):
+    for j in range(z):
+        print(z-j-i, end=" ")
+    print()
+
+z = 5 
+for i in range(z):
+    for j in range(z):
+        print(z-j, end=" ")
+    print()
+
+a = 5 
+for i in range(a):
+    for j in range(a):
+        print(j+1, end=" ")
+    print()
+
+b = 10
+for i in range(b):
+    for j in range(b):
+        print(max(i+1, j+1, n-i, n-j), end=" ")
+    print()
+
+d = 8
+for i in range(d):
+    for j in range(d):
+        i,j
+        print((i,j),end=" ")
+    print()
+
+x = 5
+for i in range(x):
+    for j in range(x):
+        print(i, end=" ")
+    print()
+
+y = 5 
+for i in range(y):
+    for j in range(y):
+        print(j, end=" ")
+    print()
+
+for i in range(y):
+    for j in range(y):
+        print(max(i,j), end = " ")
+    print()
+
+for i in range(y):
+    for j in range(y):
+        print(n-i, end = " ")
+    print()
+
+for i in range(y):
+    for j in range(y):
+        print(n-j-i, end = " ")
+    print()
+
+for i in range(y):
+    for j in range(y):
+        print(max(n-j-i, n-i-1), end = " ")
+    print()
+
+print(sum([98,76,7,86,9,4,6,56]))
